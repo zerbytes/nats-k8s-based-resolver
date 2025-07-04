a5b5c07c5efafab248bdc6e4c4c6259577b9516f
Alexander Trost <galexrt@googlemail.com>
2025-07-04 18:26:54 +0200

    fix: e2e test failing due to log line changes
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -203,7 +203,7 @@ var _ = Describe("Manager", Ordered, func() {
 				cmd := exec.Command("kubectl", "logs", controllerPodName, "-n", namespace)
 				output, err := utils.Run(cmd)
 				g.Expect(err).NotTo(HaveOccurred())
-				g.Expect(output).To(ContainSubstring("controller-runtime.metrics\tServing metrics server"),
+				g.Expect(output).To(ContainSubstring("Serving metrics server"),
 					"Metrics server not yet started")
 			}
 			Eventually(verifyMetricsServerStarted).Should(Succeed())
